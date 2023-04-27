edb84aa837d7299d6dc7805953285a4d657346d9
Vedant0616 <87979963+Vedant0616@users.noreply.github.com>
2023-04-27 14:28:47 +0530

    Added Bipartite Matching

--- ford_fulkerson.cpp
+++ ford_fulkerson.cpp
@@ -1,60 +1,70 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-map<int, char> mp;
 
-void print(vector<vector<pair<int, pair<int, int>>>> &G){
-    for(int i = 0; i<G.size(); i++){
-        cout << mp[i] << " -> ";
-        for(int j = 0; j<G[i].size(); j++){
-            cout << mp[G[i][j].first] << " " << G[i][j].second.first << " " << G[i][j].second.second << ", ";
+void print(vector<vector<pair<int, pair<int, int>>>> &G)
+{
+    for (int i = 0; i < G.size(); i++)
+    {
+        for (int j = 0; j < G[i].size(); j++)
+        {
+            cout << G[i][j].first << " " << G[i][j].second.first << " " << G[i][j].second.second << ", ";
         }
         cout << endl;
     }
 }
 
-void printResidual(vector<vector<pair<int, pair<int, bool>>>> &rG){
-    for(int i = 0; i<rG.size(); i++){
-        cout << mp[i] << " -> ";
-        for(int j = 0; j<rG[i].size(); j++){
-            cout << mp[rG[i][j].first] << " " << rG[i][j].second.first << " " << rG[i][j].second.second << ", ";
+void printResidual(vector<vector<pair<int, pair<int, bool>>>> &rG)
+{
+    for (int i = 0; i < rG.size(); i++)
+    {
+        for (int j = 0; j < rG[i].size(); j++)
+        {
+            cout << rG[i][j].first << " " << rG[i][j].second.first << " " << rG[i][j].second.second << ", ";
         }
         cout << endl;
     }
 }
 
 vector<int> parent;
-vector<vector<pair<int, pair<int, bool>>>> generateResidual(vector<vector<pair<int, pair<int, int>>>> &G){
+vector<vector<pair<int, pair<int, bool>>>> generateResidual(vector<vector<pair<int, pair<int, int>>>> &G)
+{
     vector<vector<pair<int, pair<int, bool>>>> rG(G.size());
-    for(int i = 0; i<G.size(); i++){
-        for(int j = 0; j<G[i].size(); j++){
+    for (int i = 0; i < G.size(); i++)
+    {
+        for (int j = 0; j < G[i].size(); j++)
+        {
             int v = G[i][j].first;
             int f = G[i][j].second.first;
             int c = G[i][j].second.second;
-            if(c-f > 0)
-                rG[i].push_back({v, {c-f, false}});
-            if(f > 0)
+            if (c - f > 0)
+                rG[i].push_back({v, {c - f, false}});
+            if (f > 0)
                 rG[v].push_back({i, {f, true}});
         }
     }
     return rG;
 }
 
-bool bfs(vector<vector<pair<int, pair<int, bool>>>> &rG, int s, int t){
+bool bfs(vector<vector<pair<int, pair<int, bool>>>> &rG, int s, int t)
+{
     parent.assign(rG.size(), -1);
     vector<bool> visited(rG.size(), false);
     queue<int> q;
     q.push(s);
     visited[s] = true;
     parent[s] = -1;
-    while(!q.empty()){
+    while (!q.empty())
+    {
         int u = q.front();
         q.pop();
-        if(u==t)
+        if (u == t)
             break;
-        for(int i = 0; i<rG[u].size(); i++){
+        for (int i = 0; i < rG[u].size(); i++)
+        {
             int v = rG[u][i].first;
             int f = rG[u][i].second.first;
-            if(!visited[v] && f > 0){
+            if (!visited[v] && f > 0)
+            {
                 q.push(v);
                 parent[v] = u;
                 visited[v] = true;
@@ -64,39 +74,50 @@ bool bfs(vector<vector<pair<int, pair<int, bool>>>> &rG, int s, int t){
     return visited[t];
 }
 
-void decrease(vector<vector<pair<int, pair<int, int>>>> &G, int u, int v, int bottleneck){
-    for(int i = 0; i<G[u].size(); i++){
-        if(G[u][i].first == v){
+void decrease(vector<vector<pair<int, pair<int, int>>>> &G, int u, int v, int bottleneck)
+{
+    for (int i = 0; i < G[u].size(); i++)
+    {
+        if (G[u][i].first == v)
+        {
             G[u][i].second.first -= bottleneck;
             break;
         }
     }
 }
 
-void increase(vector<vector<pair<int, pair<int, int>>>> &G, int u, int v, int bottleneck){
-    for(int i = 0; i<G[u].size(); i++){
-        if(G[u][i].first == v){
+void increase(vector<vector<pair<int, pair<int, int>>>> &G, int u, int v, int bottleneck)
+{
+    for (int i = 0; i < G[u].size(); i++)
+    {
+        if (G[u][i].first == v)
+        {
             G[u][i].second.first += bottleneck;
             break;
         }
     }
 }
 
-int ford_fulkerson(vector<vector<pair<int, pair<int, int>>>> &G, int s, int t){
+int ford_fulkerson(vector<vector<pair<int, pair<int, int>>>> &G, int s, int t)
+{
     int n = G.size();
     vector<vector<pair<int, pair<int, bool>>>> rG = generateResidual(G);
-    while(true){
+    while (true)
+    {
         // printResidual(rG);
-        if(!bfs(rG, s, t))
+        if (!bfs(rG, s, t))
             break;
         // for(int i = 0; i<n; i++)
         //     cout<<parent[i]<< " ";
         // cout<<endl;
         int bottleneck = INT_MAX;
-        for(int i = t; i!=s; i = parent[i]){
+        for (int i = t; i != s; i = parent[i])
+        {
             int u = parent[i];
-            for(int j = 0; j<rG[u].size(); j++){
-                if(rG[u][j].first == i){
+            for (int j = 0; j < rG[u].size(); j++)
+            {
+                if (rG[u][j].first == i)
+                {
                     bottleneck = min(bottleneck, rG[u][j].second.first);
                     break;
                 }
@@ -104,13 +125,19 @@ int ford_fulkerson(vector<vector<pair<int, pair<int, int>>>> &G, int s, int t){
         }
         // cout<<bottleneck<<endl;
         // printResidual(rG);
-        for(int i = t; i!=s; i = parent[i]){
+        for (int i = t; i != s; i = parent[i])
+        {
             int u = parent[i];
-            for(int j = 0; j<rG[u].size(); j++){
-                if(rG[u][j].first == i){
-                    if(rG[u][j].second.second){
+            for (int j = 0; j < rG[u].size(); j++)
+            {
+                if (rG[u][j].first == i)
+                {
+                    if (rG[u][j].second.second)
+                    {
                         decrease(G, i, u, bottleneck);
-                    }else{
+                    }
+                    else
+                    {
                         increase(G, u, i, bottleneck);
                     }
                 }
@@ -119,24 +146,38 @@ int ford_fulkerson(vector<vector<pair<int, pair<int, int>>>> &G, int s, int t){
         rG = generateResidual(G);
     }
     int max_flow = 0;
-    for(int i = 0; i<G[s].size(); i++){
+    for (int i = 0; i < G[s].size(); i++)
+    {
         max_flow += G[s][i].second.first;
     }
     return max_flow;
 }
-
-int main(){
+void bipartite()
+{
     freopen("input.txt", "r", stdin);
-    mp[0] = 's';
-    mp[1] = 'a';
-    mp[2] = 'b';
-    mp[3] = 'c';
-    mp[4] = 'd';
-    mp[5] = 't';
+    int n, m,k;
+    cin >> n >> m >> k; 
+    vector<vector<pair<int, pair<int, int>>>> G(n+ m + 2);
+    for (int i = 0; i < k; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        G[u].push_back({n+v, {0, 1}});
+    }
+    for(int i = 0; i<n; i++)
+        G[0].push_back({i+1, {0, 1}});
+    for(int i = 0; i<m; i++)
+        G[n+i+1].push_back({n+m+1, {0, 1}});
+    cout << ford_fulkerson(G, 0, n+m+1) << endl;
+}
+void normal()
+{
+    freopen("input.txt", "r", stdin);
     int n, m;
     cin >> n >> m;
     vector<vector<pair<int, pair<int, int>>>> G(n);
-    for(int i = 0; i < m; i++){
+    for (int i = 0; i < m; i++)
+    {
         int u, v, w;
         cin >> u >> v >> w;
         G[u].push_back({v, {0, w}});
@@ -145,5 +186,10 @@ int main(){
     cin >> s >> t;
     // print(G);
     cout << ford_fulkerson(G, s, t) << endl;
+}
+int main()
+{
+    // normal();
+    bipartite();
     return 0;
 }
